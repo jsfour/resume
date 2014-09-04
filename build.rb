b7cf47bcce08ddad6be7f1b3523138f16ca54e35
Jimi Smoot <jsfour@gmail.com>
2014-09-03 17:45:22 -0700

    email support

--- build.rb
+++ build.rb
@@ -1,8 +1,21 @@
 #!/usr/bin/env ruby
+require 'uri'
 
 def run_pandoc type, file_name
   return `pandoc src/JimiSmootResume.md -o #{file_name}` if type == :pdf
   return `pandoc -f markdown-yaml_metadata_block -t #{type.to_s} -o #{file_name} src/JimiSmootResume.md`
+end
+
+def build_link email, subject, message
+  message = URI.escape(message) 
+  return %[mailto:#{email}?subject=#{subject}&body=#{message}] #TODO: Move link generation into a gem
+end
+
+def build_readme email, subject
+  file = IO.read('./src/README.md')
+  message = IO.read('./src/email_template.md')
+  file.gsub('{{EMAIL-LINK}}', %[[Email me to let me know if you have a position available](#{build_link(email, subject, message)})])
+  IO.write('./README.md', file)
 end
 
 types = [:docx, :html, :pdf]
@@ -12,3 +25,6 @@ types.each do |type|
   puts file_name
   run_pandoc type, file_name
 end
+
+
+
